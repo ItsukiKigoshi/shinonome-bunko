ff77ad2f23ee4daa5a3f4f0815d646d2d23c5a2c
ItsukiKigoshi <itsukikigoshi@gmail.com>
2025-01-24 22:36:57 +0900

    chore: add unversioned files

---
+++ shinonome-web/nuxt.config.ts
@@ -0,0 +1,23 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+import vuetify, { transformAssetUrls } from 'vite-plugin-vuetify'
+export default defineNuxtConfig({
+  build: {
+    transpile: ['vuetify'],
+  },
+  compatibilityDate: '2024-11-01',
+  devtools: { enabled: true },
+  modules: [
+    (_options, nuxt) => {
+      nuxt.hooks.hook('vite:extendConfig', (config) => {
+        // @ts-expect-error
+        config.plugins.push(vuetify({ autoImport: true }))
+      })
+    },],
+  vite: {
+    vue: {
+      template: {
+        transformAssetUrls,
+      },
+    },
+  },
+})
---
+++ shinonome-web/pages/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-responsive class="border rounded" max-height="300">
+    <v-app>
+      <v-app-bar>
+        <v-app-bar-title>東雲文庫/Shinonome Bunko</v-app-bar-title>
+      </v-app-bar>
+      <v-navigation-drawer>
+        <v-list>
+          <v-list-item title="Navigation drawer"></v-list-item>
+        </v-list>
+      </v-navigation-drawer>
+      <v-main>
+        <v-container>
+          <h1>Hello, this is a Nuxt App for 東雲文庫</h1>
+        </v-container>
+      </v-main>
+    </v-app>
+  </v-responsive>
+</template>
