42ead9db79820eba3f06368482d7dc866c26b915
ItsukiKigoshi <itsukikigoshi@gmail.com>
2025-02-04 22:45:23 +0900

    feat: Replace Nuxt with Next.js But the error still happens

---
+++ src/app/mirador/page.tsx
@@ -0,0 +1,30 @@
+"use client";
+import Mirador from "mirador/dist/es/src/index";
+
+const config = {
+  id: "root",
+  miradorDownloadPlugin: {
+    restrictDownloadOnSizeDefinition: true,
+  },
+  windows: [
+    {
+      loadedManifest: "https://iiif.ub.uni-leipzig.de/0000031264/manifest.json",
+    },
+    {
+      loadedManifest: "https://scta.info/iiif/graciliscommentary/lon/manifest",
+      view: "book",
+      canvasIndex: 3,
+    },
+    {
+      loadedManifest: "https://purl.stanford.edu/xh756kf1140/iiif/manifest",
+    },
+    {
+      loadedManifest:
+        "https://digital.library.villanova.edu/Item/vudl:24299/Manifest",
+    },
+  ],
+};
+
+const miradorViewer = Mirador.viewer(config, []);
+
+export default miradorViewer;
