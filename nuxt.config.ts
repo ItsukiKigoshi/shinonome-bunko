dfb1fe5954d8374ac36b58ab5e32dff53cac1226
ItsukiKigoshi <itsukikigoshi@gmail.com>
2025-02-04 15:04:29 +0900

    chore: Update vuetify plugin and add baseURL configuration

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,18 +1,23 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
-import vuetify, { transformAssetUrls } from 'vite-plugin-vuetify'
+import vuetify, { transformAssetUrls } from "vite-plugin-vuetify";
 export default defineNuxtConfig({
-  build: {
-    transpile: ['vuetify'],
+  app: {
+    baseURL: process.env.NUXT_APP_BASE_URL || "/",
   },
-  compatibilityDate: '2024-11-01',
+  build: {
+    transpile: ["vuetify"],
+  },
+  compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
   modules: [
     (_options, nuxt) => {
-      nuxt.hooks.hook('vite:extendConfig', (config) => {
+      nuxt.hooks.hook("vite:extendConfig", (config) => {
         // @ts-expect-error
-        config.plugins.push(vuetify({ autoImport: true }))
-      })
-    },],
+        config.plugins.push(vuetify({ autoImport: true }));
+      });
+    },
+  ],
+  ssr: false, // GitHub Pages is for static sites
   vite: {
     vue: {
       template: {
@@ -20,4 +25,4 @@ export default defineNuxtConfig({
       },
     },
   },
-})
+});
