b9554519d19b0c6198da4f0da97820190e76a33d
ItsukiKigoshi <itsukikigoshi@gmail.com>
2025-02-05 22:26:12 +0900

    feat: Update Tify component and page layout
    - Update Tify component to use axios for fetching data
    - Update page layout to display current page number and provide navigation buttons for previous and next pages

--- app.vue
+++ app.vue
@@ -1,7 +1,19 @@
 <template>
   <NuxtLayout>
-    <v-app>
-      <NuxtPage />
-    </v-app>
+    <v-responsive class="border rounded">
+      <v-app>
+        <v-app-bar>
+          <v-app-bar-title>東雲文庫/Shinonome Bunko</v-app-bar-title>
+          <v-btn
+            href="https://github.com/itsukikigoshi/shinonome-bunko"
+            icon="mdi-github"
+            target="_blank"
+          ></v-btn>
+        </v-app-bar>
+        <v-main>
+          <NuxtPage />
+        </v-main>
+      </v-app>
+    </v-responsive>
   </NuxtLayout>
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,44 +1,73 @@
-<script setup>
-const items = [
-  'Item 1',
-  'Item 2',
-  'Item 3',
-  'Item 4',
-]
+<script>
+import axios from 'axios';
+import "tify";
+import "tify/dist/tify.css";
+
+export default {
+data() {
+    return {
+      text: '',
+      items: ['master', 'develop'],
+      page: 1,
+      tify: null
+    };
+  },
+  methods: {
+    async fetchData() {
+      try {
+        // TODO - ページ更新ごとにfetchするのは非効率なので、キャッシュする
+        // APIのURLにGETリクエストを送信
+        const response = await axios.get('https://lab.ndl.go.jp/dl/api/book/fulltext-json/1823865');
+        const object = response.data.list.find(item => item.page === this.page);
+        console.log(JSON.stringify(object.contents));
+        // APIから返ってきたデータをtextにセット
+        // this.text = JSON.stringify(response.data);
+        this.text = object.contents;
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    },
+    changePage(page) {
+      this.fetchData();
+      this.tify.setPage(page);
+    },
+  },
+  mounted() {
+    this.fetchData();
+    this.tify = new Tify({
+      manifestUrl: "https://www.dl.ndl.go.jp/api/iiif/1823865/manifest.json",
+      viewer: {
+        immediateRender: false,
+      },
+    });
+    this.tify.mount('#tify');
+  }
+}
 </script>
 <template>
-  <v-responsive class="border rounded">
-    <v-app>
-      <v-app-bar>
-        <v-app-bar-title>東雲文庫/Shinonome Bunko</v-app-bar-title>
-        <v-btn
-        href="https://github.com/itsukikigoshi/shinonome-bunko"
-        icon="mdi-github"
-        target="_blank"
-        ></v-btn>
-      </v-app-bar>
-      <v-main>
-        <v-container>
-          <v-row>
-            <v-col cols="8">
-              <Tify />
-            </v-col>
-            <v-col cols="4">
-              <form>
-                <v-text-field label="DOI" model-value="10.11501/1823865" readonly></v-text-field>
-                <v-text-field label="Title" model-value="蝸牛考 (言語誌叢刊)" readonly></v-text-field>
-                <v-select
-                    :items="items"
-                    label="Branch"
-                ></v-select>
-                <v-textarea auto-grow label="Text"></v-textarea>
-                <v-text-field label="Commit Message"></v-text-field>
-                <v-btn>Commit</v-btn>
-              </form>
-            </v-col>
-          </v-row>
-        </v-container>
-      </v-main>
-    </v-app>
-  </v-responsive>
+  
+  <v-container>
+    <v-row>
+      <v-col cols="8">
+        <p>Current Page: {{ this.page }}</p>
+          <div id="tify" style="height: 80vh"></div>                
+          <v-btn @click="page += 1; changePage(this.page);">Next</v-btn>
+        <v-btn @click="page -= 1; changePage(this.page);">Previous</v-btn>    
+      </v-col>
+      <v-col cols="4">
+        <form>
+          <v-text-field label="DOI" model-value="10.11501/1823865" readonly></v-text-field>
+          <v-text-field label="Title" model-value="蝸牛考 (言語誌叢刊)" readonly></v-text-field>
+          <v-select
+              :items="items"
+              label="Branch"
+          ></v-select>
+          <v-textarea label="Text" :model-value="text"
+          ></v-textarea>
+          <v-text-field label="Commit Message"></v-text-field>
+          <v-btn>Commit</v-btn>
+        </form>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
