8029614b54a854fb798e469ed922c4c5202bc959
ItsukiKigoshi <itsukikigoshi@gmail.com>
2025-02-05 14:31:30 +0900

    chore: Update router base to '/shinonome-bunko/' in nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,7 +2,7 @@ const routerBase =
   process.env.DEPLOY_ENV === 'GH_PAGES'
     ? {
         router: {
-          base: "/nuxt-mirador/"
+          base: '/shinonome-bunko/',
         },
       }
     : {}
