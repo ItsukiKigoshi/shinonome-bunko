3766029ea74046ad68fd943d6f4dae89082f3f9b
ItsukiKigoshi <itsukikigoshi@gmail.com>
2025-02-05 17:17:54 +0900

    add Tify

--- pages/index.vue
+++ pages/index.vue
@@ -20,13 +20,10 @@ const items = [
       <v-main>
         <v-container>
           <v-row>
-            <h1>東雲文庫</h1>
-          </v-row>
-          <v-row>
-            <v-col cols="6">
+            <v-col cols="8">
               <Tify />
             </v-col>
-            <v-col cols="6">
+            <v-col cols="4">
               <form>
                 <v-text-field label="DOI" model-value="10.11501/1823865" readonly></v-text-field>
                 <v-text-field label="Title" model-value="蝸牛考 (言語誌叢刊)" readonly></v-text-field>
