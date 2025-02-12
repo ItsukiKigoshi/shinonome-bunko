54a2f300ba43a9bc356b36ff77a1fdde47f1dd3d
ItsukiKigoshi <itsukikigoshi@gmail.com>
2025-02-12 19:52:09 +0900

    feat: Update pages/[org]/[id].vue to <script setup lang="ts">

--- pages/edit/[org]/[id].vue
+++ pages/edit/[org]/[id].vue
@@ -1,180 +1,164 @@
-<script>
+<script setup lang="ts">
   import axios from 'axios';
   import 'tify';
   import 'tify/dist/tify.css';
   import { Octokit } from '@octokit/rest';
   import { Buffer } from 'buffer';
-  import { config } from 'dotenv';
+  const route = useRoute();
+  let tify;
+  onMounted(() => {
+    tify = new Tify({
+      manifestUrl: `https://www.dl.ndl.go.jp/api/iiif/${route.params.id}/manifest.json`,
+      viewer: {
+        immediateRender: false
+      }
+    });
+    tify.mount('#tify');
+    fetchData();
+  });
 
-  export default {
-    data() {
-      return {
-        cachedResponse: {},
-        commiter: {
-          name: '',
-          email: ''
-        },
-        commitDestination: '',
-        commitMessage: '',
-        text: '',
-        tify: null
-      };
-    },
-    methods: {
-      async fetchData() {
-        try {
-          const owner = 'itsukikigoshi';
-          const repo = 'shinonome-storage';
-          this.tify.ready.then(async () => {
-            const path =
-              this.$route.params.org +
-              '/' +
-              this.$route.params.id +
-              '/' +
-              this.tify.options.pages[0] +
-              '.txt';
-            const octokit = new Octokit({
-              auth: config.githubOauthToken
-            });
-            let file;
-            try {
-              file = await octokit.rest.repos.getContent({
-                owner: owner,
-                repo: repo,
-                path: path
-              });
-            } catch (e) {
-              if (e.status !== 404) {
-                throw e;
-              }
-              file = null;
-            }
-            // When file exists on GitHub, use it as the initial text
-            if (file) {
-              console.log('file exists');
-              const github_response = await axios.get(file.data.download_url);
-              console.log(github_response.data);
-              this.text = github_response.data;
-            } else if (this.cachedResponse.length > 0) {
-              console.log('cached response');
-              // Use cached response if available
-              this.tify.ready.then(() => {
-                this.text = this.cachedResponse.find(
-                  item => item.page === this.tify.options.pages[0]
-                ).contents;
-              });
-            } else {
-              console.log('fetching data');
-              // Fetch data from API
-              const ndl_response = await axios.get(
-                'https://lab.ndl.go.jp/dl/api/book/fulltext-json/1823865'
-              );
-              const object = ndl_response.data.list;
-              // Cache the response
-              this.cachedResponse = object;
-              // Set the text
-              this.tify.ready.then(() => {
-                this.text = object.find(
-                  item => item.page === this.tify.options.pages[0]
-                ).contents;
-              });
-            }
-            this.tify.ready.then(() => {
-              this.commitDestination =
-                'https://github.com/itsukikigoshi/shinonome-storage/blob/main/' +
-                this.$route.params.org +
-                '/' +
-                this.$route.params.id +
-                '/' +
-                this.tify.options.pages[0] +
-                '.txt';
-              this.commitMessage =
-                'Update ' +
-                this.$route.params.org +
-                '/' +
-                this.$route.params.id +
-                '/' +
-                this.tify.options.pages[0] +
-                '.txt';
-            });
-          });
-        } catch (error) {
-          console.error('Error fetching data:', error);
-        }
-      },
-      changePage(num) {
-        this.tify.ready.then(() => {
-          if (this.tify.options.pages[0] + num > 0) {
-            this.tify.setPage(this.tify.options.pages[0] + num);
-            this.fetchData();
-          }
+  // Is it a good use case of "let" to declare these variables?
+  const text = defineModel('text', {
+    type: String,
+    default: ''
+  });
+  const commiterName = defineModel('commiterName', {
+    type: String,
+    default: ''
+  });
+  const commiterEmail = defineModel('commiterEmail', {
+    type: String,
+    default: ''
+  });
+  const commitMessage = defineModel('commitMessage', {
+    type: String,
+    default: ''
+  });
+  const cachedResponse = defineModel('cachedResponse', {
+    type: Object,
+    default: {}
+  });
+  const commitDestination = defineModel('commitDestination', {
+    type: String,
+    default: ''
+  });
+
+  const fetchData = () => {
+    try {
+      const owner = 'itsukikigoshi';
+      const repo = 'shinonome-storage';
+      tify.ready.then(async () => {
+        const path = `${route.params.org}/${route.params.id}/${tify.options.pages[0]}.txt`;
+        const octokit = new Octokit({
+          auth: '' // TODO - Add GitHub OAuth token
         });
-      },
-      createGitHubCommit() {
-        const owner = 'itsukikigoshi';
-        const repo = 'shinonome-storage';
-        this.tify.ready.then(async () => {
-          const path =
-            this.$route.params.org +
-            '/' +
-            this.$route.params.id +
-            '/' +
-            this.tify.options.pages[0] +
-            '.txt';
-          const octokit = new Octokit({
-            auth: config.githubOauthToken
-          });
-          let file;
-          try {
-            file = await octokit.rest.repos.getContent({
-              owner: owner,
-              repo: repo,
-              path: path
-            });
-          } catch (e) {
-            if (e.status !== 404) {
-              throw e;
-            }
-            file = null;
-          }
-
-          const response = await octokit.repos.createOrUpdateFileContents({
+        let file;
+        try {
+          file = await octokit.rest.repos.getContent({
             owner: owner,
             repo: repo,
-            path: path,
-            content: Buffer.from(this.text).toString('base64'),
-            message: this.commitMessage,
-            committer: {
-              name: this.commiter.name,
-              email: this.commiter.email
-            },
-            author: {
-              name: this.commiter.name,
-              email: this.commiter.email
-            },
-            sha: file ? file.data.sha : null
+            path: path
           });
-          console.log(response);
-        });
-      }
-    },
-    mounted() {
-      // TODO - Make a function to add manifestUrl based on whether the book is in the NDL or not
-      this.tify = new Tify({
-        manifestUrl:
-          'https://www.dl.ndl.go.jp/api/iiif/' +
-          this.$route.params.id +
-          '/manifest.json',
-        viewer: {
-          immediateRender: false
+        } catch (e) {
+          if (e.status !== 404) {
+            throw e;
+          }
+          file = null;
         }
+        // When file exists on GitHub, use it as the initial text
+        if (file) {
+          console.log('file exists');
+          const github_response = await axios.get(file.data.download_url);
+          console.log(github_response.data);
+          text.value = github_response.data;
+        } else if (cachedResponse.value.length > 0) {
+          console.log('cached response');
+          // Use cached response if available
+          tify.ready.then(() => {
+            text.value = cachedResponse.value.find(
+              item => item.page === tify.options.pages[0]
+            ).contents;
+          });
+        } else {
+          console.log('fetching data');
+          // Fetch data from API
+          const ndl_response = await axios.get(
+            `https://lab.ndl.go.jp/dl/api/book/fulltext-json/${route.params.id}`
+          );
+          const object = ndl_response.data.list;
+          // Cache the response
+          cachedResponse.value = object;
+          // Set the text
+          tify.ready.then(() => {
+            text.value = object.find(
+              item => item.page === tify.options.pages[0]
+            ).contents;
+          });
+        }
+        tify.ready.then(() => {
+          commitDestination.value = `https://github.com/itsukikigoshi/shinonome-storage/blob/main/${route.params.org}/${route.params.id}/${tify.options.pages[0]}.txt`;
+          commitMessage.value = `Update ${route.params.org}/${route.params.id}/${tify.options.pages[0]}.txt`;
+        });
       });
-      this.tify.mount('#tify');
-      this.fetchData();
+    } catch (error) {
+      console.error('Error fetching data:', error);
     }
+  };
+
+  const changePage = num => {
+    tify.ready.then(() => {
+      if (tify.options.pages[0] + num > 0) {
+        tify.setPage(tify.options.pages[0] + num);
+        fetchData();
+      }
+    });
+  };
+
+  const createGitHubCommit = () => {
+    const owner = 'itsukikigoshi';
+    const repo = 'shinonome-storage';
+    tify.ready.then(async () => {
+      const path = `${route.params.org}/${route.params.id}/${tify.options.pages[0]}.txt`;
+      const octokit = new Octokit({
+        auth: '' // TODO - Add GitHub OAuth token
+      });
+      let file;
+      try {
+        file = await octokit.rest.repos.getContent({
+          owner: owner,
+          repo: repo,
+          path: path
+        });
+      } catch (e) {
+        if (e.status !== 404) {
+          throw e;
+        }
+        file = null;
+      }
+
+      const response = await octokit.repos.createOrUpdateFileContents({
+        owner: owner,
+        repo: repo,
+        path: path,
+        content: Buffer.from(text).toString('base64'),
+        message: commitMessage,
+        committer: {
+          name: commiterName,
+          email: commiterEmail
+        },
+        author: {
+          name: commiterName,
+          email: commiterEmail
+        },
+        sha: file ? file.data.sha : null
+      });
+      console.log(response);
+    });
   };
 </script>
 <template>
+  <p>{{ route.params.id }}</p>
+  <p>Your Name is {{ commiterName }}</p>
   <v-container>
     <v-row>
       <v-col cols="6">
@@ -197,21 +181,21 @@
           <v-text-field
             label="Page"
             :model-value="
-              this.tify &&
-              this.tify.options &&
-              this.tify.options.pages &&
-              this.tify.options.pages[0]
+              tify &&
+              tify.options &&
+              tify.options.pages &&
+              tify.options.pages[0]
             "
             readonly
           ></v-text-field>
           <v-textarea label="Text" v-model="text"></v-textarea>
           <v-text-field
             label="Commiter Name"
-            v-model="commiter.name"
+            v-model="commiterName"
           ></v-text-field>
           <v-text-field
             label="Commiter Email"
-            v-model="commiter.email"
+            v-model="commiterEmail"
           ></v-text-field>
           <v-textarea
             label="Commit Message"
@@ -223,8 +207,8 @@
         </form>
         <p>
           Commit Destination:
-          <NuxtLink :to="this.commitDestination" target="_blank">
-            {{ this.commitDestination }}
+          <NuxtLink :to="commitDestination" target="_blank">
+            {{ commitDestination }}
           </NuxtLink>
         </p>
       </v-col>
